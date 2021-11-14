23a6c1e669bfaf186ce6310827b4f1b8dd139797
jqsheng <jqsheng@trip.com>
2021-11-15 01:02:46 +0800

    feat: 优化02题目的 js逻辑

--- 02-progress-steps/script.js
+++ 02-progress-steps/script.js
@@ -1,14 +1,25 @@
-let percent = 0;
+let actived = 1;
+const CIRCLE_COUNT = 4;
+
+const circles = document.querySelectorAll('.circle');
+const btnNext = document.querySelector('#next');
+const btnPrev = document.querySelector('#prev');
+const progress = document.querySelector('#progress');
+
 document.querySelectorAll('.btn').forEach(item => {
     item.addEventListener('click', function (event) {
         const id = event.target.id;
-        if (id === 'next' && percent < 3) {
-            percent++;
-            document.querySelectorAll('.circle')[percent].classList.add('active');
-        } else if (id === 'prev' && percent > 0) {
-            document.querySelectorAll('.circle')[percent].classList.remove('active');
-            percent--;
+        if (id === 'next' && actived < CIRCLE_COUNT) {
+            actived++;
+            circles[actived - 1].classList.add('active');
+            event.target.disabled = (actived === CIRCLE_COUNT);
+            btnPrev.disabled = false;
+        } else if (id === 'prev' && actived > 1) {
+            circles[actived - 1].classList.remove('active');
+            actived--;
+            event.target.disabled = (actived === 1);
+            btnNext.disabled = false;
         }
-        document.querySelector('#progress').style.width = (33.33 * percent).toFixed(2) + '%';
+        progress.style.width = (100 / (CIRCLE_COUNT - 1) * (actived - 1)).toFixed(2) + '%';
     });
 });
